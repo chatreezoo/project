18d9f2b2c526780147be6ce808fd72f290c955e7
chatreezoo <60030213+chatreezoo@users.noreply.github.com>
2020-01-18 17:01:05 +0700

    Add files via upload

---
+++ Lab3/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		int age = 24;
+		double gpa=3.00;
+		String name="Ketsaraqporn Puengphuk";
+		char gender='F';
+		
+		System.out.println("This is programming");
+		System.out.println("Name : "+ name+ "\nAge : "+age+"\nGender : "+ gender +"\ngpa : "+gpa);
+
+	}
+
+}
